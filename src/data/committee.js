d7b8d22bff9fed83ad8e80353105c05c0ad417c1
kwabenankyi <anthony.nkyi@outlook.com>
2024-10-21 00:50:16 +0100

    added Sofiya (blog writer) to about us page

--- src/data/committee.js
+++ src/data/committee.js
@@ -56,6 +56,14 @@ const committee = {
             ai_concept: '',
             fact: '',
         },
+        {
+          name: 'Sofiya Flenova',
+          position: 'Blog Writer',
+          image: 'sofiya_flenova.jpeg',
+          course: 'MEng Robotics and AI (1st Year)',
+          ai_concept: '',
+          fact: '',
+        },
         // {
         //   name: 'Charlie Harrison',
         //   position: 'Content Executive',
